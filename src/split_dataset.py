8bb224dc02b8383ec05b0cb59e53b7c6591fa8c8
junesnow86 <junliang@mail.ustc.edu.cn>
2024-02-02 17:04:37 +0000

    reduce validation and test size to accelerate bleu calculation

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -8,7 +8,7 @@ df = pd.read_csv('/home/ljt/DL-exp5/data/news-commentary-v15/news-commentary-v15
 df = df.sample(frac=0.1, random_state=42)
 
 # Split the DataFrame into training and temp DataFrames
-train_df, temp_df = train_test_split(df, test_size=0.4, random_state=42)
+train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
 
 # Split the temp DataFrame into validation and testing DataFrames
 val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
