9128952059def358f419285018aede0aa30e127a
junesnow86 <junliang@mail.ustc.edu.cn>
2024-02-02 17:05:28 +0000

    a no-problem version

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,121 @@
 import torch
 from tqdm import tqdm
-from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
+from data_utils import (
+    BOS_IDX,
+    EOS_IDX,
+    PAD_IDX,
+    SRC_LANGUAGE,
+    TGT_LANGUAGE,
+    MAX_LEN,
+)
 
-def evaluate(model, val_dataloader, references):
-    pass
+@torch.no_grad()
+def ids2sentence(token_ids: torch.Tensor, vocab_transform):
+    sentence = " ".join(vocab_transform.lookup_tokens(list(token_ids.cpu().numpy()))).replace("<bos>", "").replace("<eos>", "")
+    return sentence
+
+@torch.no_grad()
+def get_batch_references(batch_token_ids, vocab_transform):
+    references = []
+    for token_ids in batch_token_ids:
+        reference = ids2sentence(token_ids, vocab_transform)
+        references.append(reference)
+    return references
+
+@torch.no_grad()
+def translate_helper(model, src_tensor_batch, tgt_tensor_batch):
+    '''
+    translate a batch of src tensor to tgt tensor
+    '''
+    model.eval()
+    for i in range(MAX_LEN - 1):
+        tgt_hat = model(src_tensor_batch, tgt_tensor_batch)
+        for j in range(len(src_tensor_batch)):
+            tgt_tensor_batch[j, i] = torch.argmax(tgt_hat[j, i-1])
+            if torch.equal(tgt_tensor_batch[j, i], torch.tensor(EOS_IDX).to(tgt_tensor_batch.device)):
+                break
+    return tgt_tensor_batch
+
+@torch.no_grad()
+def translate(model, src_sentence_batch, text_transform, tgt_vocab_transform):
+    '''
+    translate a batch of src sentences to tgt sentences
+    '''
+    src_tensor_batch = []
+    for src_sentence in src_sentence_batch:
+        src_tensor = text_transform[SRC_LANGUAGE](src_sentence)
+        src_tensor_batch.append(src_tensor)
+    src_tensor_batch = torch.stack(src_tensor_batch)
+    tgt_tensor_batch = torch.ones(len(src_sentence_batch), MAX_LEN).fill_(BOS_IDX).type(torch.long)
+
+    tgt_tensor_batch = translate_helper(model, src_tensor_batch, tgt_tensor_batch)
+
+    output_sentences = get_batch_references(tgt_tensor_batch, tgt_vocab_transform)
+    return output_sentences
+
+@torch.no_grad()
+def evaluate_batch(model, src_tensor_batch, tgt_tensor_batch, tgt_vocab_transform):
+    '''
+    translate a batch of src tensor to tgt tensor
+    convert tgt tensor to tgt sentence as reference
+    use model to translate logits tensor to hypothesis
+    '''
+    model.eval()
+
+    references = get_batch_references(tgt_tensor_batch, tgt_vocab_transform)
+
+    tgt_for_hypo = torch.ones(len(tgt_tensor_batch), MAX_LEN).fill_(BOS_IDX).type(torch.long).to(src_tensor_batch.device)
+    hypotheses = translate_helper(model, src_tensor_batch, tgt_for_hypo)
+    hypotheses = get_batch_references(hypotheses, tgt_vocab_transform)
+
+    # Calculate BLEU score
+    assert len(references) == len(hypotheses), f'len(references)={len(references)}, len(hypotheses)={len(hypotheses)}'
+    total_score = 0.0
+    for reference, hypothesis in zip(references, hypotheses):
+        reference = reference.split()
+        hypothesis = hypothesis.split()
+        score = sentence_bleu([reference], hypothesis, smoothing_function=SmoothingFunction().method4)
+        total_score += score
+
+    return total_score / len(references)
+
+@torch.no_grad()
+def evaluate(model, val_dataloader, tgt_vocab_transform, device='cuda'):
+    '''
+    val_dataloader: iter batches of (src tensor, tgt tensor)
+    convert tgt tensor to tgt sentence as reference
+    use model to translate logits tensor to hypothesis
+    '''
+    model.to(device)
+    model.eval()
+    total_score = 0.0
+
+    import time
+    progress_bar = tqdm(val_dataloader, total=len(val_dataloader), desc='evaluation')
+    for src, tgt in progress_bar:
+        src = src.long().to(device)
+        tgt = tgt.long().to(device)
+
+        tic = time.time()
+        references = get_batch_references(tgt, tgt_vocab_transform)
+        toc = time.time()
+        print(f'get_batch_references takes {toc - tic:.4f} seconds')
+
+        tgt_for_hypo = torch.ones(len(tgt), MAX_LEN).fill_(BOS_IDX).type(torch.long).to(src.device)
+        tic = time.time()
+        hypotheses = translate_helper(model, src, tgt_for_hypo)
+        toc = time.time()
+        print(f'translate_helper takes {toc - tic:.4f} seconds')
+        hypotheses = get_batch_references(hypotheses, tgt_vocab_transform)
+
+        # Calculate BLEU score
+        assert len(references) == len(hypotheses), f'len(references)={len(references)}, len(hypotheses)={len(hypotheses)}'
+        for reference, hypothesis in zip(references, hypotheses):
+            reference = reference.split()
+            hypothesis = hypothesis.split()
+            score = sentence_bleu([reference], hypothesis, smoothing_function=SmoothingFunction().method4)
+            total_score += score
+
+    return total_score / len(val_dataloader)
--- src/train.py
+++ src/train.py
@@ -2,9 +2,18 @@ import torch
 from tqdm import tqdm
 
 from data_utils import PAD_IDX
+from evaluate import evaluate_batch, evaluate
 
 
-def train(model, optimizer, train_dataloader, loss_fn, device, num_epochs, scheduler=None):
+def train(model, 
+          optimizer, 
+          train_dataloader, 
+          val_dataloader,
+          tgt_vocab_transform,
+          loss_fn, 
+          device, 
+          num_epochs, 
+          scheduler=None):
     model.to(device)
     for epoch in range(1, num_epochs+1):
         progress_bar = tqdm(train_dataloader, total=len(train_dataloader), desc='training')
@@ -20,12 +29,12 @@ def train(model, optimizer, train_dataloader, loss_fn, device, num_epochs, sched
 
             tgt_label_mask = tgt_label != PAD_IDX
             preds = torch.argmax(logits, -1)
-            correct = (preds == tgt_label).float()
+            correct = preds == tgt_label
             acc = torch.sum(correct * tgt_label_mask) / torch.sum(tgt_label_mask)
 
             n, seq_len = tgt_label.shape
-            logits = logits.reshape(n * seq_len, -1)
-            tgt_label = tgt_label.reshape(n * seq_len)
+            logits = torch.reshape(logits, (n * seq_len, -1))
+            tgt_label = torch.reshape(tgt_label, (n * seq_len, ))
             loss = loss_fn(logits, tgt_label)
 
             if scheduler is not None:
@@ -37,29 +46,34 @@ def train(model, optimizer, train_dataloader, loss_fn, device, num_epochs, sched
             optimizer.step()
 
             # Update tqdm progress bar with the latest loss value
-            progress_bar.set_postfix({'loss': f'{loss.item()/len(src):.4f}'})
-            progress_bar.set_postfix({'acc': f'{acc.item():.4f}'})
+            progress_bar.set_postfix({'loss': f'{loss.item()/len(src):.4f}', 'acc': f'{acc:.4f}'})
+
+            # Calculate training BLEU score
+            # bleu_score_train = evaluate_batch(model, src, tgt, tgt_vocab_transform)
+            # progress_bar.set_postfix({'bleu': f'{bleu_score_train:.4f}'})
 
     return model.to('cpu')
 
 
 if __name__ == '__main__':
-    NUM_EPOCH = 10
-    BATCH_SIZE = 64
-    LR = 0.001
-    MAX_LEN = 120
+    NUM_EPOCH = 5
+    BATCH_SIZE = 16
+    LR = 0.0001
+    MAX_LEN = 200
     d_model = 512
     d_ff = 2048
     n_layer = 6
     n_head = 8
-    dropout = 0.1
+    dropout = 0.2
 
     # Set seed
-    seed = 10
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+    # seed = 10
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
 
-    # Tokenizers
+    # Load vocab
+    import os
+
     from torchtext.data.utils import get_tokenizer
 
     from data_utils import (
@@ -69,23 +83,20 @@ if __name__ == '__main__':
         create_data_iter,
         sequential_transforms,
         tensor_transform,
-        truncate_transform
+        truncate_transform,
     )
 
-    token_transform = {}
-    token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_trf')
-    token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='zh_core_web_trf')
-
-    # Load vocab
-    import os
     vocab_save_dir = '/home/ljt/DL-exp5/vocab/news-commentary-v15'
     vocab_transform = {}
-    for ln in ['en', 'zh']:
+    for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
         vocab_file = os.path.join(vocab_save_dir, f'vocab_{ln}.pt')
         vocab_transform[ln] = torch.load(vocab_file)
     SRC_VOCAB_SIZE = len(vocab_transform[SRC_LANGUAGE])
     TGT_VOCAB_SIZE = len(vocab_transform[TGT_LANGUAGE])
 
+    token_transform = {}
+    token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_trf')
+    token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='zh_core_web_trf')
     # ``src`` and ``tgt`` language text transforms to convert raw strings into tensors indices
     text_transform = {}
     for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
@@ -105,18 +116,37 @@ if __name__ == '__main__':
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     from seq2seq_network import MyTransformer
-    model = MyTransformer(SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, PAD_IDX, n_layer, n_head, d_model, d_ff, dropout)
-
+    model = MyTransformer(SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, 0, n_layer, n_head, d_model, d_ff, dropout)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.9, patience=10, verbose=True)
-    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95, verbose=True)
-
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=PAD_IDX)
 
-    trained_model = train(model, optimizer, train_dataloader, loss_fn, DEVICE, NUM_EPOCH, scheduler=scheduler)
-    torch.save(trained_model.state_dict(), '../checkpoints/model.pth')
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.9, patience=10, verbose=True)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95, verbose=True)
 
-    from inference import translate
-    src_sentence = 'The school lunch program is the largest discrete market for low-cost, healthy food.'
-    translate(model, src_sentence, text_transform, vocab_transform, DEVICE)
+    print('>>> Start training...')
+    train(
+        model=model, 
+        optimizer=optimizer, 
+        train_dataloader=train_dataloader, 
+        val_dataloader=None,
+        tgt_vocab_transform=vocab_transform[TGT_LANGUAGE],
+        loss_fn=loss_fn, 
+        device=DEVICE, 
+        num_epochs=NUM_EPOCH)
+    # bleu_score = evaluate(model, train_dataloader, vocab_transform[TGT_LANGUAGE], DEVICE)
+    # print(f'>>> Training finished. Train BLEU score: {bleu_score:.4f}')
+    print(f'>>> Training finished.')
+    torch.save(model.state_dict(), '../checkpoints/model.pth')
+
+    # Validation
+    val_iter = create_data_iter('/home/ljt/DL-exp5/data/news-commentary-v15/val.tsv')
+    val_dataloader = DataLoader(list(val_iter), 
+                                  batch_size=BATCH_SIZE, 
+                                  shuffle=True, 
+                                  collate_fn=lambda data: collate_fn(data, text_transform))
+    bleu_score = evaluate(model, val_dataloader, vocab_transform[TGT_LANGUAGE], DEVICE)
+    print(f'Validation BLEU score: {bleu_score:.4f}')
+
+    # from inference import translate
+    # src_sentence = 'The school lunch program is the largest discrete market for low-cost, healthy food.'
+    # translate(model, src_sentence, text_transform, vocab_transform, DEVICE)
